aeeddd73c39e2f18f999fd810a745807afe65b82
Clara <clamcgrew98@gmail.com>
2024-10-01 11:18:28 -0400

    Working on the Register view.

--- lib/Views/Login.dart
+++ lib/Views/Login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../components/login_text.dart';
 import '../components/login_textfield.dart';
 import '../components/flutter_button.dart';
@@ -7,69 +6,69 @@ import 'Home.dart';
 import 'Register.dart';
 class Login extends StatelessWidget {
   Login({super.key});
-final userController = TextEditingController();
-final passwordController = TextEditingController();
+
+  final userController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
-Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-          child: Column(children:  [
-            const SizedBox(height: 50),
-        //Logo
-        const Icon(Icons.add_box, size:75,),
+        body: SafeArea(
+            child: Center(
+                child: Column(children: [
+                  const SizedBox(height: 50),
+                  //Logo
+                  const Icon(Icons.add_box, size: 75,),
 
-        //username textfield
-      LoginTextfield(
-        controller: userController,
-        hintText: 'Enter your username or email',
-        obscureText: false,
-      ),
-const SizedBox(height: 10),
-        //password textfield
-            LoginTextfield(
-              controller: passwordController,
-            hintText: 'Enter your password',
-              obscureText: true,
-            ),
-        const SizedBox(height:10),
+                  //username textfield
+                  LoginTextfield(
+                    controller: userController,
+                    hintText: 'Enter your username or email',
+                    obscureText: false,
+                  ),
+                  const SizedBox(height: 10),
+                  //password textfield
+                  LoginTextfield(
+                    controller: passwordController,
+                    hintText: 'Enter your password',
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 10),
 
-        //forgot password?
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal:25.0),
-           child: Row(mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-            Text('Forgot password?', style: TextStyle(color:Colors.grey[600]),
-            ),
-        ],
-            ),
-            ),
-            const SizedBox(height:25),
-              //sign in button
-            FlutterButton(
-              onTap: (){
-                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context)=> const Home()));
-              }
-            ),
+                  //forgot password?
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text('Forgot password?',
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  //sign in button
+                  FlutterButton(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (BuildContext context) => const Home()));
+                      }
+                  ),
 
-        //Don't have an account? Register here
-            const SizedBox(height:10),
-      LoginText(
-        onTap: (){
-          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context)=> const Register()));
-        },
-      )
+                  //Don't have an account? Register here
+                  const SizedBox(height: 10),
+                  LoginText(
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (BuildContext context) => Register()));
+                    },
+                  )
 
 
+                ],)
 
-
-
-      ],)
-
-    )
-    ));
-}
-
+            )
+        ));
+  }
 
 }
--- lib/Views/Register.dart
+++ lib/Views/Register.dart
@@ -1,9 +1,65 @@
 import 'package:flutter/material.dart';
+import '../components/flutter_button.dart';
+import '../components/login_textfield.dart';
+import 'Home.dart';
+
 class Register extends StatelessWidget {
-  const Register({super.key});
+  Register({super.key});
+  final userController = TextEditingController();
+  final passwordController = TextEditingController();
+  final retypePasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: SafeArea(
+        child: Center(
+        child: Column(children:  [
+        const SizedBox(height: 50),
+    //Logo
+    const Icon(Icons.add_box, size:75,),
+
+    //username textfield
+    LoginTextfield(
+    controller: userController,
+    hintText: 'Enter your username',
+    obscureText: false,
+    ),
+    const SizedBox(height: 10),
+          //username textfield
+          LoginTextfield(
+            controller: userController,
+            hintText: 'Enter your email',
+            obscureText: false,
+          ),
+          const SizedBox(height: 10),
+
+    //password textfield
+    LoginTextfield(
+    controller: passwordController,
+    hintText: 'Enter your password',
+    obscureText: true,
+    ),
+    const SizedBox(height:10),
+          //password textfield
+          LoginTextfield(
+            controller: retypePasswordController,
+            hintText: 'Re-type your password',
+            obscureText: true,
+          ),
+          const SizedBox(height:10),
+          FlutterButton(
+              onTap: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (BuildContext context) => const Home()));
+              }
+          ),
+
+]
+    ),
+        ),
+    ),
+    );
   }
 
 }
